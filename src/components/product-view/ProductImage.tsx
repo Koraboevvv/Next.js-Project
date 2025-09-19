874a50c694d78053e1a19a2895cee819ffd27e6b
Koraboev.vvv <bexruzkarabev@gmail.com>
2025-09-19 17:46:00 +0500

    70%

--- src/components/product-view/ProductImage.tsx
+++ src/components/product-view/ProductImage.tsx
@@ -10,10 +10,10 @@ const ProductImage: FC<{ title: string, image: string, id: number }> = (props) =
     <div>
       <Image
         src={"https://api.errorchi.uz/product/image/" + image}
-        width={350}
+        width={295}
         alt={title}
         height={300}
-        className="w-full h-[300px] object-cover rounded-lg"
+        className="w-full h-[350px] object-cover rounded-lg"
          onClick={() => router.push(`/product/${id}`)}
       />
     </div>
--- src/components/product-view/ProductView.tsx
+++ src/components/product-view/ProductView.tsx
@@ -1,41 +1,53 @@
 import { FC, memo } from 'react';
 import ProductImage from './ProductImage';
+import { Rate } from 'antd';
+
+
 
 const ProductView: FC<{ data: any[] }> = ({ data }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-      {data?.map((item: any) => {
-        const price = item.price;
-        const oldPrice = Math.round(price / 0.7);
-        const discount = Math.round(((oldPrice - price) / oldPrice) * 100);
+    <div className="p-4 ">
+      <h1 className="font-black text-[48px] text-center mt-[72px] mb-[55px]">NEW ARRIVALS</h1>
 
-        return (
-          <div
-            key={item.id}
-            className="bg-white border-0 rounded-[10px] shadow-sm hover:shadow-lg transition-shadow duration-300 cursor-pointer h-[400px]"
-          >
-            <ProductImage
-              title={item.title}
-              image={item.images[0]}
-              id={item.id}
-            />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {data?.map((item: any) => {
+          const price = item.price;
+          const oldPrice = Math.round(price / 0.7);
+          const discount = Math.round(((oldPrice - price) / oldPrice) * 100);
 
-            <div className="p-4">
-              <h3 className="text-lg font-semibold text-gray-800 truncate">
-                {item.title}
-              </h3>
-              <hr />
-              <div className="flex items-center space-x-2">
-                <span className="text-lg font-bold text-black">${price}</span>
-                <span className="text-gray-400 line-through">${oldPrice}</span>
-                <span className="bg-red-100 text-red-500 text-xs font-semibold px-2 py-0.5 rounded-full ">
-                  -{discount}%
-                </span>
+          return (
+            <div
+              key={item.id}
+              className="bg-white border-0 rounded-[10px] shadow-sm hover:shadow-lg transition-shadow duration-300 cursor-pointer h-[500px]"
+            >
+              <ProductImage
+                title={item.title}
+                image={item.images[0]}
+                id={item.id}
+              />
+
+              <div className="p-4">
+                <h3 className="text-lg font-semibold text-gray-800 truncate">
+                  {item.title}
+                </h3>
+                <Rate allowHalf defaultValue={5} className="text-yellow-500 text-sm" />
+
+
+                <br />
+                <br />
+                <hr />
+                <div className="flex items-center space-x-2 mt-2">
+                  <span className="text-lg font-bold text-black">${price}</span>
+                  <span className="text-gray-400 line-through">${oldPrice}</span>
+                  <span className="bg-red-100 text-red-500 text-xs font-semibold px-2 py-0.5 rounded-full">
+                    -{discount}%
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
